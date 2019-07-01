e32d02795b5fbf760839977ac9d1d44b358e7f59
Jackson Cole <jackson@jacksoncole.io>
2019-07-01 11:07:49 -0500

    Set up polybar to work with multiple monitors

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -7,6 +7,12 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config
+if [ $(command -v xrandr) ]; then
+    for m in $(xrandr --query | grep " connected" | cut -d " " -f1); do
+        MONITOR=$m polybar --reload example &
+    done
+else
 polybar example &
+fi
 
 echo "Polybar launched..."
